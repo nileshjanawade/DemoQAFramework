389b38b1c5d094200be2f7508b3d3bcec91e6e2f
Nilesh <neel.janawade@gmail.com>
2022-07-06 19:14:09 +0530

    deleted few lines from Base Page

--- src/main/java/resources/BasePage.java
+++ src/main/java/resources/BasePage.java
@@ -7,8 +7,6 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
 import java.io.FileInputStream;
